abbbb109ab93e55df283e1f8c9bc6db337658090
jstakun <jstakun@redhat.com>
2021-01-28 10:16:46 +0100

    minor update

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+kubectl delete helloquarkus --all
+kubectl delete namespace hello-quarkus-operator
+kubectl delete clusterrole hello-quarkus-operator 
+kubectl delete crd  helloquarkus.operator.waw.redhat.pl
